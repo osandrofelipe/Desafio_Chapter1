a6e6bc66e05908cf97a11a930f2bf12640063631
Sandro Felipe <sandrokbca@hotmail.com>
2022-05-23 10:31:30 -0300

    Fix field blank

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,6 +16,9 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if (!newTaskTitle) return // Não libera campo vazio
+   
     const newTask = {
       id: Math.random(),
       title: newTaskTitle,
